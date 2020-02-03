91bb3c27454d9c5fdf0e8ca55710cdacd6135133
mattbradburyuk <matthew.bradbury@me.com>
2020-02-03 13:52:16 +0000

    removed deal contract

--- contracts/src/main/kotlin/com/template/states/AccountsDealState.kt
+++ contracts/src/main/kotlin/com/template/states/AccountsDealState.kt
@@ -1,12 +1,10 @@
 package com.template.states
 
 import com.template.contracts.AccountsDealContract
-import com.template.contracts.DealContract
 import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.ContractState
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.AnonymousParty
-import net.corda.core.identity.Party
 
 
 @BelongsToContract(AccountsDealContract::class)
